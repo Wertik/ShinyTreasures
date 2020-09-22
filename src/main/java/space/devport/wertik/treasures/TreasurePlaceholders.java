b88a809c61ea0f4fe715bf303114b010194afa90
Wertik <jregvx@gmail.com>
2020-09-22 20:00:26 +0200

    Add top placeholders.

--- src/main/java/space/devport/wertik/treasures/TreasurePlaceholders.java
+++ src/main/java/space/devport/wertik/treasures/TreasurePlaceholders.java
@@ -8,7 +8,12 @@ import space.devport.wertik.treasures.system.tool.struct.PlacementTool;
 import space.devport.wertik.treasures.system.treasure.struct.Treasure;
 import space.devport.wertik.treasures.system.user.struct.User;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
 import java.util.function.Predicate;
+import java.util.function.ToIntFunction;
+import java.util.stream.Collectors;
 
 public class TreasurePlaceholders extends PlaceholderExpansion {
 
@@ -19,17 +24,13 @@ public class TreasurePlaceholders extends PlaceholderExpansion {
     }
 
     /*
-     * %treasures_found%
-     * %treasures_found_<template>%
-     * %treasures_found_tool_<tool>%
+     * %treasures_found_(template/<tool:<toolName>>)%
      *
-     * %treasures_placed%
-     * %treasures_placed_<template>%
-     * %treasures_places_tool_<tool>%
+     * %treasures_placed_(template/<tool:<toolName>>)%
      *
-     * %treasures_hidden%
-     * %treasures_hidden_<template>%
-     * %treasures_hidden_tool_<tool>%
+     * %treasures_hidden_(template/<tool:<toolName>>)%
+     *
+     * %treasures_top_<position>_<name/count>_(template/<tool:<toolName>>)%
      * */
 
     @Override
@@ -48,34 +49,70 @@ public class TreasurePlaceholders extends PlaceholderExpansion {
         if (args[0].equalsIgnoreCase("found")) {
             return getCount(args, user::hasFound);
         } else if (args[0].equalsIgnoreCase("placed")) {
-            return getCount(args, (treasure) -> true);
+            return getCount(args, treasure -> true);
         } else if (args[0].equalsIgnoreCase("hidden")) {
-            return getCount(args, (treasure) -> !user.hasFound(treasure));
+            return getCount(args, treasure -> !user.hasFound(treasure));
+        } else if (args[0].equalsIgnoreCase("top")) {
+
+            if (args.length < 3)
+                return "not_enough_args";
+
+            int position = ParserUtil.parseInt(args[1]);
+            if (position <= 0)
+                return "invalid_position";
+
+            List<User> top = getTop(plugin.getUserManager().getUsers(u -> true));
+
+            if (top.size() < position)
+                return "invalid_position";
+
+            User topUser = top.get(position - 1);
+
+            if (args[2].equalsIgnoreCase("name")) {
+                return topUser.getOfflinePlayer().getName();
+            } else if (args[2].equalsIgnoreCase("count")) {
+                return String.valueOf(topUser.getFindCount());
+            }
         }
         return "invalid_params";
     }
 
-    private String getCount(String[] args, Predicate<Treasure> userCondition) {
+    private List<User> getTop(Set<User> users) {
+        return users.stream()
+                .sorted(Comparator.comparingInt((ToIntFunction<User>) User::getFindCount).reversed())
+                .collect(Collectors.toList());
+    }
 
-        TreasureTemplate template = args.length > 1 ? plugin.getTemplateManager().getTemplate(args[1]) : null;
-        PlacementTool tool = args.length > 2 ? plugin.getToolManager().getTool(args[2]) : null;
+    private Predicate<Treasure> parseTypeCondition(String arg) {
+        Predicate<Treasure> typeCondition;
+        if (arg.toLowerCase().startsWith("tool:")) {
+            PlacementTool tool = plugin.getToolManager().getTool(arg.replace("tool:", ""));
+            if (tool == null)
+                return null;
+            typeCondition = treasure -> treasure.getTool().equals(tool);
+        } else {
+            TreasureTemplate template = plugin.getTemplateManager().getTemplate(arg);
+            if (template == null)
+                return null;
+            typeCondition = treasure -> treasure.getTool().getRootTemplate() != null &&
+                    treasure.getTool().getRootTemplate().equals(template);
+        }
+        return typeCondition;
+    }
+
+    private String getCount(String[] args, Predicate<Treasure> userCondition) {
 
         if (args.length == 1)
             return getCount(userCondition);
 
-        if (args[1].equalsIgnoreCase("tool")) {
-            if (tool == null)
-                return "invalid_tool";
+        Predicate<Treasure> typeCondition = parseTypeCondition(args[1]);
 
-            return getCount((treasure) -> treasure.getTool() != null && treasure.getTool().equals(tool) && userCondition.test(treasure));
-        }
+        if (typeCondition == null)
+            return "invalid_filter";
 
-        if (template == null)
-            return "invalid_template";
-
-        return getCount((treasure) -> treasure.getTool() != null &&
-                treasure.getTool().getRootTemplate() != null &&
-                treasure.getTool().getRootTemplate().equals(template) && userCondition.test(treasure));
+        return getCount(treasure -> treasure.getTool() != null &&
+                typeCondition.test(treasure) &&
+                userCondition.test(treasure));
     }
 
     private String getCount(Predicate<Treasure> condition) {
